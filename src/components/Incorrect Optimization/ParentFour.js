5a1578c001861f2cba57c7d90ca912172e8d8b1d
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-07 17:47:29 +0530

    use of useMemo in case of object to optimized React Rendering

--- src/components/Incorrect Optimization/ParentFour.js
+++ src/components/Incorrect Optimization/ParentFour.js
@@ -1,25 +1,26 @@
-import React,{useState} from 'react'
+import React,{useState,useMemo} from 'react'
 import { MemoizedChildFive } from './ChildFive'
 
 export const ParentFour = () => {
     const [count,setCount]=useState(0)
     const [name,setName]=useState('Rishabh')
 
-    // const person={
-    //     fname:'Rishabh',
-    //     lname:'Jaiswal'
-    // }
-
-    const handleClick=()=>{
-
+    const person={
+        fname:'Rishabh',
+        lname:'Jaiswal'
     }
+
+    const MemoizedPerson=useMemo(()=>person,[])
+
+    // const handleClick=()=>{}
     console.log('parentFour Render')
   return (
     <div>
         <button onClick={()=>setCount(c=>c+1)}>Count-{count}</button>
         <button onClick={()=>setName('Lucky')}>ChangeName</button>
         {/* <MemoizedChildFive name={name} person={person}/> */}
-        <MemoizedChildFive name={name} handleClick={handleClick}/>
+        <MemoizedChildFive name={name} person={MemoizedPerson}/>
+        {/* <MemoizedChildFive name={name} handleClick={handleClick}/> */}
     </div>
   )
 }
