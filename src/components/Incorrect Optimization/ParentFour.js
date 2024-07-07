2bd3016861792869f7b8f4e100971477e7ade536
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-07 17:51:42 +0530

    use of useCallback in case of function to optimized React Rendering

--- src/components/Incorrect Optimization/ParentFour.js
+++ src/components/Incorrect Optimization/ParentFour.js
@@ -1,26 +1,30 @@
-import React,{useState,useMemo} from 'react'
+import React,{useState,useMemo,useCallback} from 'react'
 import { MemoizedChildFive } from './ChildFive'
 
 export const ParentFour = () => {
     const [count,setCount]=useState(0)
     const [name,setName]=useState('Rishabh')
 
-    const person={
-        fname:'Rishabh',
-        lname:'Jaiswal'
-    }
+    // const person={
+    //     fname:'Rishabh',
+    //     lname:'Jaiswal'
+    // }
 
-    const MemoizedPerson=useMemo(()=>person,[])
+//    const MemoizedPerson=useMemo(()=>person,[])
 
-    // const handleClick=()=>{}
+     const handleClick=()=>{}
+
+     const MemoizedHandleClick=useCallback(handleClick,[])
+
     console.log('parentFour Render')
   return (
     <div>
         <button onClick={()=>setCount(c=>c+1)}>Count-{count}</button>
         <button onClick={()=>setName('Lucky')}>ChangeName</button>
         {/* <MemoizedChildFive name={name} person={person}/> */}
-        <MemoizedChildFive name={name} person={MemoizedPerson}/>
+        {/* <MemoizedChildFive name={name} person={MemoizedPerson}/> */}
         {/* <MemoizedChildFive name={name} handleClick={handleClick}/> */}
+        <MemoizedChildFive name={name} handleClick={MemoizedHandleClick}/>
     </div>
   )
 }
